8d8c9c45b5d5c442beb74dafde3cf9ba01d9cc31
abiramdinesh1998 <dinesh.chittamsetty@gmail.com>
2023-07-27 13:08:23 +0530

    first commit

--- app1/views.py
+++ app1/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def data_render(request):
-    d = {'name':'subbu'}
+    d = {'name':'subbu','age':90}
     return render(request,'data_render.html',context=d)
